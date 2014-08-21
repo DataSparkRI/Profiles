10ac9712aa0e8efc8d0946291b8dece528bf753b
Yang <yxu@provplan.org>
2014-08-21 14:29:01 -0400

    make short definition in search term

--- communityprofiles/profiles/admin.py
+++ communityprofiles/profiles/admin.py
@@ -268,7 +268,7 @@ class IndicatorAdmin(SortableAdmin):
     prepopulated_fields = {"slug": ("name",)}
     list_filter = (LastGeneratedDateField, NextUpdateDateField,'published','data_type', 'indicatorpart__data_source', 'indicatorpart__time', 'data_domains__domain')
     list_display = ('name','published','display_name','levels_str', 'data_type', 'sources_str', 'times_str', 'domains_str', 'short_definition', 'last_generated_at','last_modified_at')
-    search_fields = ['display_name', 'name', 'id']
+    search_fields = ['display_name', 'name', 'id', 'short_definition']
     # There seems to a bug in Django admin right now, which prevents making these fields editable
     #list_editable = ('short_definition', 'long_definition',)
     inlines = [
