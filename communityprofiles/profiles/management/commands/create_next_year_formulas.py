42683f627872d7ed5b623f19194ddbc9b617ed1c
Yang <yxu@provplan.org>
2016-12-19 10:49:17 -0500

    update formulas

--- communityprofiles/profiles/management/commands/create_next_year_formulas.py
+++ communityprofiles/profiles/management/commands/create_next_year_formulas.py
@@ -11,6 +11,13 @@ class Command(BaseCommand):
         new_data_source_name = 'CensusAPI_ACS5_2010' # already created or auto created
         new_data_source_implementation = 'data_adapters.CensusAPI_ACS5_2010'
         
+        '''
+        copy_data_source_name = 'CensusAPI_ACS5_2012_Profile' # already created
+        new_time_name = '2011-2015'
+        new_time_sort = '2015'
+        new_data_source_name = 'CensusAPI_ACS5_2015_Profile' # already created or auto created
+        new_data_source_implementation = 'data_adapters.CensusAPI_ACS5_2015_Profile'
+        '''
         
         time, created = Time.objects.get_or_create(name = new_time_name, sort = new_time_sort)
         datasource_copy_from = DataSource.objects.get(name = copy_data_source_name)
@@ -21,24 +28,25 @@ class Command(BaseCommand):
            
            new_indicator_part, created = IndicatorPart.objects.get_or_create(indicator = indicator_part.indicator, 
                                               time = time,
-                                              data_source = datasource_copy_to,
-                                              formula = indicator_part.formula,
-                                              data = indicator_part.data,
-                                              published = indicator_part.published)
-           new_indicator_part.levels = indicator_part.levels.all()
-           new_indicator_part.save()
-           
-           indicator = indicator_part.indicator        
+                                              data_source = datasource_copy_to)
+           if created:
+               new_indicator_part.formula = indicator_part.formula
+               new_indicator_part.data = indicator_part.data
+               new_indicator_part.published = True #indicator_part.published
+               new_indicator_part.levels = indicator_part.levels.all()
+               new_indicator_part.save()
+               
+               indicator = indicator_part.indicator        
 
-           denominator_parts = DenominatorPart.objects.filter(indicator = indicator, data_source = datasource_copy_from, part = indicator_part)
-           for denominator_part in denominator_parts:
-               new_denominator_part, created = DenominatorPart.objects.get_or_create(indicator = denominator_part.indicator, 
-                               denominator = denominator_part.denominator,
-                               part = new_indicator_part,
-                               data_source = datasource_copy_to,
-                               formula = denominator_part.formula,
-                               data = denominator_part.data,
-                               published = denominator_part.published)
-               new_denominator_part.levels = denominator_part.levels.all()
-               new_denominator_part.save()
+               denominator_parts = DenominatorPart.objects.filter(indicator = indicator, data_source = datasource_copy_from, part = indicator_part)
+               for denominator_part in denominator_parts:
+                   new_denominator_part, created = DenominatorPart.objects.get_or_create(indicator = denominator_part.indicator, 
+                                   denominator = denominator_part.denominator,
+                                   part = new_indicator_part,
+                                   data_source = datasource_copy_to,
+                                   formula = denominator_part.formula,
+                                   data = denominator_part.data,
+                                   published = denominator_part.published)
+                   new_denominator_part.levels = denominator_part.levels.all()
+                   new_denominator_part.save()
 
